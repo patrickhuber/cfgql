7b4e06ca0eb203c0b1dcde126093dd981bbe8f14
patrickhuber <patrickhuber@users.noreply.github.com>
2020-02-11 08:42:10 -0500

    adds app query

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/99designs/gqlgen v0.10.2
-	github.com/cloudfoundry-community/go-cfclient v0.0.0-20190808214049-35bcce23fc5f // indirect
+	github.com/cloudfoundry-community/go-cfclient v0.0.0-20190808214049-35bcce23fc5f
 	github.com/vektah/gqlparser v1.2.0
 	gopkg.in/yaml.v2 v2.2.8
 )
--- resolver.go
+++ resolver.go
@@ -4,8 +4,12 @@ package cfgql
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 
+	"github.com/cloudfoundry-community/go-cfclient"
 	"github.com/patrickhuber/cfgql/exec"
 	"github.com/patrickhuber/cfgql/models"
 	"gopkg.in/yaml.v2"
@@ -29,17 +33,10 @@ func (r *Resolver) Foundation() exec.FoundationResolver {
 type queryResolver struct{ *Resolver }
 
 func (r *queryResolver) Foundations(ctx context.Context) ([]*models.Foundation, error) {
-	dat, err := ioutil.ReadFile("foundations.yml")
+	connections, err := connections(ctx)
 	if err != nil {
 		return nil, err
 	}
-
-	var connections []*Connection
-	err = yaml.Unmarshal(dat, &connections)
-	if err != nil {
-		return nil, err
-	}
-
 	foundations := []*models.Foundation{}
 	for _, connection := range connections {
 		foundation := &models.Foundation{
@@ -50,14 +47,68 @@ func (r *queryResolver) Foundations(ctx context.Context) ([]*models.Foundation, 
 	return foundations, nil
 }
 
+func connections(ctx context.Context) ([]*Connection, error) {
+	dat, err := ioutil.ReadFile("foundations.yml")
+	if err != nil {
+		return nil, err
+	}
+
+	var connections []*Connection
+	err = yaml.Unmarshal(dat, &connections)
+	return connections, err
+}
+
 type foundationResolver struct{ *Resolver }
 
 func (r *foundationResolver) Applications(ctx context.Context, f *models.Foundation) ([]*models.Application, error) {
-	id := "abc123"
-	name := "hello"
-	application := &models.Application{
-		ID:   &id,
-		Name: &name,
+
+	connections, err := connections(ctx)
+	if err != nil {
+		return nil, err
 	}
-	return []*models.Application{application}, nil
+
+	var connection *Connection
+	for _, conn := range connections {
+		if strings.Compare(*conn.ID, *f.ID) == 0 {
+			connection = conn
+		}
+	}
+	if connection == nil {
+		return nil, fmt.Errorf("unable to find connection for foundation %s", *f.ID)
+	}
+	if connection.API == nil {
+		return nil, fmt.Errorf("missing connection api for foundation %s", *f.ID)
+	}
+
+	username := os.Getenv("CF_USERNAME")
+	password := os.Getenv("CF_PASSWORD")
+
+	c := &cfclient.Config{
+		ApiAddress:        *connection.API,
+		Username:          username,
+		Password:          password,
+		SkipSslValidation: true,
+	}
+
+	client, err := cfclient.NewClient(c)
+	if err != nil {
+		return nil, err
+	}
+
+	apps, err := client.ListApps()
+	if err != nil {
+		return nil, err
+	}
+
+	applications := []*models.Application{}
+	for _, app := range apps {
+		id := app.Guid
+		name := app.Name
+		application := &models.Application{
+			ID:   &id,
+			Name: &name,
+		}
+		applications = append(applications, application)
+	}
+	return applications, nil
 }
